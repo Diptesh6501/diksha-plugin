ff0e7f0ef92955b9f973a8b4dcd29eaf2022bb73
diptesh6501 <diptesh1993@gmail.com>
2019-03-12 15:21:33 +0530

    Issue #SB-10822 refactoring the routes and controller files

--- server/controllers/framework.ts
+++ server/controllers/framework.ts
@@ -4,9 +4,21 @@ export class FrameWork {
     constructor() { }
 
     public async getFramework(req, res) {
-        res.json({
-            'message': 'getframework'
-        })
-
+        const framework = req.params;
+        if (framework) {
+            try {
+                const data = fs.readFileSync(__dirname + `./data/framework/framework.${framework}.json`);
+                res.json({
+                    'status': 200,
+                    'data': data
+                })
+            }
+            catch {
+                 res.json({
+                     'status': 404,
+                     'data': 'file not found'
+                 })
+            }
+        }
     }
 }
--- server/router/framework.router.ts
+++ server/router/framework.router.ts
@@ -1,11 +1,11 @@
 import { Manifest } from '@project-sunbird/ext-framework-server/models/Manifest';
 import { frameworkAPI } from '@project-sunbird/ext-framework-server/api';
-import { FrameWork } from './controllers/framework';
+import { FrameWork } from '../controllers/framework';
 
 export class Router {
     init(app: any, manifest: Manifest, auth?: any) {
         const frameWork = new FrameWork()
         const server = frameworkAPI.getPluginInstance(manifest.id);
-        app.get('/getFramework', (req, res) => { frameWork.getFramework(req, res) })
+        app.get('/api/framework/v1/read', (req, res) => { frameWork.getFramework(req, res) })
     }
 }
