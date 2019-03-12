77cfde2b9cb359ad9d69632d84cd35bf8ce35c1b
sudip-mukherjee <sudip.mukherjee@tarento.com>
2019-03-12 17:24:14 +0530

    Issue #SB-10822 feat: Added constant file for error code and message

---
+++ server/constants/response.constant.ts
@@ -0,0 +1,12 @@
+export class ResponseCode {
+    public static readonly ERROR_200 = 200;
+    public static readonly ERROR_404 = 404;
+    public static readonly ERROR_500 = 500;
+}
+
+export class ResponseMessage {
+    public static readonly SUCCESS = "success";
+    public static readonly RESOURCE_NOT_FOUND = "resouce not found";
+    public static readonly INTERNAL_SERVER_ERROR = "internal server error";
+
+}
